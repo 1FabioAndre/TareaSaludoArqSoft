271d4e38d6d3d68500433c2a4442a98cc89bb13b
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-14 19:13:47 -0400

    Greeting Controller aniadido

---
+++ FramkyService/src/main/java/com/ucb/FramkyService/GreetingController.java
@@ -0,0 +1,16 @@
+package com.ucb.FramkyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/greet")
+public class RestController {
+    @Autowired
+    GreetingService greetingService;
+
+    @GetMapping
+    public String greeting(){
+        return greetingService.getGreeting();
+    }
+}
