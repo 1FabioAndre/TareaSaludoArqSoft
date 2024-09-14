0bcac9d284a04e110c7b22c8a21b825f05e92b5e
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-14 19:34:22 -0400

    test y greetig controller aniadido

--- FramkyService/src/main/java/com/ucb/FramkyService/GreetingService.java
+++ FramkyService/src/main/java/com/ucb/FramkyService/GreetingService.java
@@ -1,11 +1,12 @@
 package com.ucb.FramkyService;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    String greet = "Preguntate si lo que estas haciendo hoy te hacerca al lugar en el que quieres estar mañana";
+    String greet = "Preguntate si lo que estas haciendo hoy te acerca a lo que quieres hacer mañana";
 
-    public String getGreeting() {
+    public String getGreeting(){
         return greet;
     }
 }
